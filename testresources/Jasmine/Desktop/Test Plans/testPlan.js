76242bd8e4eca3aca2063d29c00d39e99bc06358
Mallesham Katnam <mallesham.katnam@gmail.com>
2021-05-25 13:11:55 -0500

    Test plan updated

--- testresources/Jasmine/Desktop/Test Plans/testPlan.js
+++ testresources/Jasmine/Desktop/Test Plans/testPlan.js
@@ -6,9 +6,17 @@ require([/*<test plan file>*/]);
 //2.  require the test suites along with executing jasmine as below
 //Nested require for test suites will ensure the order of test suite exectuion
 //and so on
-require(["/Test Suites/emp_dir_web_testsuite"], function(){
+/*require(["/Test Suites/emp_dir_web_testsuite"], function(){
 jasmine.getEnv().execute();  
-});
+});*/
 
 
 //Since this file is manually edited, make sure to cascade any changes (rename/delete) to the corresponding test suites/plans
+require([/*<Test Suites/test suite1>*/],function(){
+  require([/*<Test Suites/test suite2>*/], function(){
+    //and so on
+       require([/*<Test Suites/emp_dir_tab_testsuite>*/], function(){
+           jasmine.getEnv().execute();  
+        });
+  });
+});
