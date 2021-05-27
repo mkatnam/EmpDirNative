8d2f2225f28c87c2bea5d190a1c3b1a4fc065775
Mallesham Katnam <mallesham.katnam@gmail.com>
2021-05-26 22:45:40 -0500

    test scripts updated

--- testresources/Jasmine/Mobile/Test Plans/testPlan.js
+++ testresources/Jasmine/Mobile/Test Plans/testPlan.js
@@ -1,20 +1,20 @@
 //This is the entry point for automation. You can either:
 //1.Require any one of the created test plans like this:
-require([/*<test plan file>*/]);
+//require([/*<test plan file>*/]);
 
 // or
 //2.  require the test suites along with executing jasmine as below
 //Nested require for test suites will ensure the order of test suite exectuion
-       /* require(["/Test Suites/emp_dir_native_testsuite"], function(){
+        require(["/Test Suites/emp_dir_native_testsuite.js"], function(){
             jasmine.getEnv().execute();  
-        });*/
+        });
 
 //Since this file is manually edited, make sure to cascade any changes (rename/delete) to the corresponding test suites/plans
-require([/*<Test Suites/test suite1>*/],function(){
-  require([/*<Test Suites/test suite2>*/], function(){
+//require([/*<Test Suites/test suite1>*/],function(){
+ // require([/*<Test Suites/test suite2>*/], function(){
     //and so on
-       require([/*<Test Suites/emp_dir_tab_testsuite>*/], function(){
-           jasmine.getEnv().execute();  
-        });
-  });
-});
+      // require([/*<Test Suites/emp_dir_tab_testsuite>*/], function(){
+          // jasmine.getEnv().execute();  
+       // });
+  //});
+//}); */
